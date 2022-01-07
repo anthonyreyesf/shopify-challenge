d8057363de26b686bf7802c1511063625c9f976b
Anthony Reyes <anthonyreyesf@gmail.com>
2022-01-07 16:32:04 -0500

    implemented project fundamentals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Card from "./Components/Card";
+
+import { default as Avatar } from "./Components/Assets/avatar.png";
+import { default as Post } from "./Components/Assets/post.jpg";
+
+const API_KEY = "Z5BdQe6xe3pDMJeZHWcnmB9IBKJAKR9MLuIUpiJd";
+const myProfile = {
+  username: "anthonyreyesf",
+  caption: `Hey there, I'm Anthony Reyes and I'm currently a student @YorkU!${"\n\n"}I previously worked at @Ada, @D2L and @Mixemapp as a Software Developer Intern and I hope to be able to do my last internship at @Shopify!`,
+  numOfLikes: "2022",
+  avatar: Avatar,
+  post: Post,
+  comments: [
+    { username: "champagnepapi", comment: "Keep it up 🔥🔥🔥" },
+    { username: "nasa", comment: "To the moooon 🧑‍🚀🚀" },
+  ],
+};
 
 function App() {
+  const [nasaData, setNasaData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.nasa.gov/planetary/apod?count=6&api_key=${API_KEY}`)
+      .then((results) => results.json())
+      .then((data) => {
+        setNasaData(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 py-8">
+      <Card
+        avatar={myProfile.avatar}
+        post={myProfile.post}
+        username={myProfile.username}
+        numOfLikes={"2022"}
+        caption={myProfile.caption}
+        comments={myProfile.comments}
+      />
+      {nasaData && (
+        nasaData.map(apod => (
+          <Card
+            key={apod.title}
+            avatar={apod.url}
+            post={apod.hdurl}
+            username={apod.title}
+            numOfLikes={Math.floor(Math.random() * 100000) + 1000}
+            caption={apod.explanation}
+          />
+        ))
+      )}
     </div>
   );
 }
-
+//key={apod.title} source={apod.hdurl} title={apod.title} content={apod.explanation} date={apod.date}
 export default App;
---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { HeartIcon, ChatAltIcon } from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+
+const comments = [
+  { username: "chefcurry", comment: "Perfect pic, nice one 🔥🔥" },
+  { username: "thehoneyqueen", comment: "nice" },
+  { username: "johnlenny", comment: "Meh I can take better pictures" },
+  { username: "summertimejoe", comment: "Incredible!!" },
+  { username: "kevinyarn", comment: "we need to check this out @linuxarmy" },
+  { username: "jsonmaxie", comment: "I need this on my wall" },
+  { username: "linuxarmy", comment: "@kevinyarn you gotta see this 👀" },
+  { username: "javaexpress", comment: "So awesome" },
+];
+const comment1 = comments[Math.floor(Math.random() * comments.length)];
+const comment2 = comments[Math.floor(Math.random() * comments.length)];
+
+export default function Card(props) {
+  const [isLiked, setIsLiked] = useState(false);
+  
+  return (
+    <div className="max-w-md sm:mx-auto rounded overflow-hidden shadow-lg mx-5 mb-8 bg-white">
+      <div className="w-full flex justify-between p-3">
+        <div className="flex">
+          <div className="rounded-full h-8 w-8 bg-gray-500 flex items-center justify-center overflow-hidden">
+            <img src={props.avatar} alt="profilepic" />
+          </div>
+          <span className="pt-1 ml-2 font-bold text-sm">{props.username}</span>
+        </div>
+        <span className="px-2 hover:bg-gray-300 cursor-pointer rounded">
+          <i className="fas fa-ellipsis-h pt-2 text-lg"></i>
+        </span>
+      </div>
+      <img className="w-full bg-cover" src={props.post} />
+      <div className="px-3 pb-2">
+        <div className="pt-2">
+          <button onClick={() => setIsLiked(!isLiked)}>
+            <svg
+              className="w-5 h-5 mr-2 text-gray-600 inline-block cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              {isLiked ? (
+                <HeartIconFilled className="text-red-500" />
+              ) : (
+                <HeartIcon />
+              )}
+            </svg>
+          </button>
+          <svg
+            className="w-5 h-5 mr-2 text-gray-600 inline-block"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <ChatAltIcon />
+          </svg>
+          <span className="text-sm text-gray-400 font-medium block">
+            {props.numOfLikes} likes
+          </span>
+        </div>
+        <div className="pt-1">
+          <div className="mb-2 text-sm whitespace-pre-line">
+            <span className="font-medium mr-2">{props.username}</span>
+            {props.caption}
+          </div>
+        </div>
+        <div className="text-sm mb-2 text-gray-400 cursor-pointer font-medium">
+          View all 14 comments
+        </div>
+        <div className="mb-2">
+          {props.comments ? (
+            props.comments.map((comment) => (
+              <div className="mb-1 text-sm">
+                <span className="font-medium mr-2">{comment.username}</span>{" "}
+                {comment.comment}
+              </div>
+            ))
+          ) : (
+            <>
+              <div className="mb-1 text-sm">
+                <span className="font-medium mr-2">{comment1.username}</span> {comment1.comment}
+              </div>
+              <div className="mb-1 text-sm">
+                <span className="font-medium mr-2">{comment2.username}</span> {comment2.comment}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
